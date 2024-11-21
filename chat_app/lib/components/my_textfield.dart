d2360f820ad23be0b12973458cd829938badf77a
Kanishka Anand <kanishkaadz@gmail.com>
2024-11-21 21:32:21 +0530

    Update my_textfield.dart

--- chat_app/lib/components/my_textfield.dart
+++ chat_app/lib/components/my_textfield.dart
@@ -18,7 +18,8 @@ class MyTextField extends StatelessWidget {
           ),
           fillColor: Theme.of(context).colorScheme.secondary,
           filled: true,
-          hintText: "",
+          hintText: hintText,
+          hintStyle: TextStyle(color: Theme.of(context).colorScheme.primary),
         ),
       ),
     );
