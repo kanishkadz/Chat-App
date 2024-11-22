c0d5a281796995598dbc249155cf77f060ec1721
Kanishka Anand <kanishkaadz@gmail.com>
2024-11-22 19:53:17 +0530

    Update main.dart

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/auth/auth_gate.dart';
 import 'package:chat_app/auth/login_or_register.dart';
 import 'package:chat_app/themes/light_mode.dart';
 import 'package:flutter/material.dart';
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const LoginOrRegister(),
+      home: const AuthGate(),
       theme: lightMode,
     );
   }
