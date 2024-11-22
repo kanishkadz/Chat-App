0bfb22ff50e4394ccbb3f2075f0cfbd10a9365f7
Kanishka Anand <kanishkaadz@gmail.com>
2024-11-22 21:46:59 +0530

    Update main.dart

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:chat_app/themes/light_mode.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
 
