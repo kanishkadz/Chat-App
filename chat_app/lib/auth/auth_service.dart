b02320a2b07dac3560e7702a7572221bcf8d3bdf
Kanishka Anand <kanishkaadz@gmail.com>
2024-11-22 21:38:44 +0530

    firebase

--- chat_app/lib/auth/auth_service.dart
+++ chat_app/lib/auth/auth_service.dart
@@ -1,21 +1,27 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
-
-  //instance or auth
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  //sign in
-  Future<UserCredential> signInWithEmailPassword(String email, password) async {
-    try{
-      UserCredential userCredential = await _auth.signInWithEmailPassword(email: email, passowrd: password,);
+  Future<UserCredential> signInWithEmailPassword(String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(email: email, password: password);
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
     }
   }
 
-  //sign up
+  Future<UserCredential> signUpWithEmailPassword(String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+    }
+  }
 
-  //sign out
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
 }
