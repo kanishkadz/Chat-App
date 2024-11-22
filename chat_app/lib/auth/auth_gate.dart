f9b8dd867ed0ff3c6a1125534bd1a29477ab0042
Kanishka Anand <kanishkaadz@gmail.com>
2024-11-22 19:50:00 +0530

    Create auth_gate.dart

---
+++ chat_app/lib/auth/auth_gate.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(), 
+        builder: (context, snapshot) {
+          //user is logged in
+
+          //user is not logged in
+        }
+      ),
+    );
+  }
+}
